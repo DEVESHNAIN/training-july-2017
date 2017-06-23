8ad9ebc03f39dc88cdde53c6a20fb5382d36d05a
devesh <deveshnain@gmail.com>
2017-06-23 12:39:51 +0530

    JavaScript function to extract unique characters from a string.

---
+++ task-1/program-7/script/prog-7.js
@@ -0,0 +1,19 @@
+class uniqueChar{
+   uniqueCharacter(str){
+     let i;
+     let uChar=" ";
+     for(i=0;i<str.length;i++)
+     {
+       if((str.indexOf(str.charAt(i)) === (str.lastIndexOf(str.charAt(i))))) {
+         uChar=uChar+" '" + str.charAt(i)+"' ";
+       }
+     document.getElementById("para").innerHTML ="Unique character in String "+str+" is "+uChar;
+   }
+}
+}
+
+function myfunction() {
+  let str= document.getElementById("str").value;
+  const obj =new uniqueChar();
+  obj.uniqueCharacter(str);
+}

30bb35f4cc3f5eab7c1e3b2556dc1c34b7319752
devesh <deveshnain@gmail.com>
2017-06-23 12:26:43 +0530

    javascript code to Ask user to enter some input, check for even and odd number

---
+++ task-1/program-1/script/prog-1.js
@@ -0,0 +1,27 @@
+class checkNo{
+  constructor(num) {
+    this.num=num;
+  }
+  evenAndOdd() {
+  let no=this.num;
+  if((no % 2) == 0) {
+    document.getElementById("para").innerHTML ="the given number is even ";
+  //  document.write("the given number is even ");
+  }
+  else {
+    document.getElementById("para").innerHTML ="the given number is odd";
+  }
+}
+};
+function myfunction() {
+let num= Number(document.getElementById("no").value);
+
+      if(Number.isInteger(num)==true){
+        const obj= new checkNo(num);
+        obj.evenAndOdd();
+        }
+      else {
+        document.getElementById("para").innerHTML ="the given number is not a integer ";
+    }
+
+};

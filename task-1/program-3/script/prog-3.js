c6038a3a425f02dcd708c56d6c659d52c95dba40
devesh <deveshnain@gmail.com>
2017-06-27 18:25:06 +0530

    js program to calculate sum of All Prime numbers between 1 to 999

--- task-1/program-3/script/prog-3.js
+++ task-1/program-3/script/prog-3.js
@@ -1,11 +1,11 @@
 class operation {
-   isPrime (num) {
+   isPrime(num) {
     var i;
     if ((num==0)||(num==1))
     {
       return false;
     }
-    for (i=2;i<=(num/2);i++)
+    for(i=2;i<=(num/2);i++)
     {
       if ((num%i)==0)
       {
@@ -16,13 +16,13 @@ class operation {
   }
 };
 function myfunction () {
- let num1=Number(document.getElementById("no1").value);
- let num2=Number(document.getElementById("no2").value);
- var sum=0;
- var j;
- const obj1= new operation();
+  let num1=Number(document.getElementById("no1").value);
+  let num2=Number(document.getElementById("no2").value);
+  var sum=0;
+  var j;
+  const obj1= new operation();
   for (j=num1;j<=num2;j++) {
-      if (obj1.isPrime(j))
+    if (obj1.isPrime(j))
       {
         sum=sum+j;
       }

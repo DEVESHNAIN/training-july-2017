7ba9c21145716872a6bb10f17bc00097ed1cd2b1
Devesh Nain <devesh.n@successivesoftwares.com>
2017-07-24 19:18:57 +0530

    written TDD for subtask and tasks server side method seprated method and collection file

--- meteor/simple-todos/imports/api/tasks.tests.js
+++ meteor/simple-todos/imports/api/tasks.tests.js
@@ -3,14 +3,20 @@
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
 import { assert } from 'meteor/practicalmeteor:chai';
+import {sinon} from 'meteor/practicalmeteor:sinon';
 import { Tasks } from './tasks.js';
 
 if (Meteor.isServer) {
   describe('Tasks', () => {
     describe('methods', () => {
-      const userId = Random.id();
-      let taskId;
+      const userId = sinon.stub(Meteor, 'userId',()=>{
+        return "XJpbymbnyb9yFerHh";
+      });
+      let username= sinon.stub(Meteor, 'user.username',() => {
+        return "tmeasday";
 
+      });
+      let taskId;
       beforeEach(() => {
         Tasks.remove({});
         taskId = Tasks.insert({
@@ -26,14 +32,27 @@ if (Meteor.isServer) {
         const deleteTask = Meteor.server.method_handlers['tasks.remove'];
 
         // Set up a fake method invocation that looks like what the method expects
-        const invocation = { userId };
+        // const invocation = { userId };
 
         // Run the method with `this` set to the fake invocation
-        deleteTask.apply(invocation, [taskId]);
+        deleteTask.apply({}, [taskId]);
 
         // Verify that the method does what we expected
         assert.equal(Tasks.find().count(), 0);
       });
+
+
+
+    describe('insert', () => {
+      beforeEach(() => {
+        Tasks.remove({});
+      });
+      it('can insert task', () =>{
+        const insertTask= Meteor.server.method_handlers['tasks.insert'];
+
+        insertTask.apply({},['test-text']);
+        assert.equal(Tasks.find().count(), 1);
+      });
     });
   });
-}
+});

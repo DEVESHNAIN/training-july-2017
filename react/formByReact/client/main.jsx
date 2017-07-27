42302ab1e39af1d1dc6a163add66a4a0323fd44c
Devesh Nain <devesh.n@successivesoftwares.com>
2017-07-27 19:29:48 +0530

    created routes for form and home using react router

--- react/formByReact/client/main.jsx
+++ react/formByReact/client/main.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import App from '../imports/ui/App.jsx';
-// import Label from '../imports/ui/App.jsx';
 
 Meteor.startup(() => {
-  //const element=<Input type="text">;
-  // const App =<div>
-  //   <Input.lbl name="Name"/>
-  //   <Input.inp type="text" placeholder="enter your name" />
-  // </div>;
-
+ const el =  <Router>
+                <div>
+                  <ul>
+                    <li><Link to="/">Home</Link></li><br></br>
+                    <li><Link to="/form">Form</Link></li><br></br>
+                  </ul>
+                  <Route exact path="/form" component={App}/>
+                </div>
+              </Router>
   render(
-     <App />,
+    el,
      document.getElementById('render-target'));
 
 });
